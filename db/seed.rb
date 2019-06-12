f6bfa73a0e4e66ce861d3483906a405da203a062
github username <github email address>
2019-06-12 23:21:36 +0100

    read and parse csv data into a sqlite3 database

--- db/seed.rb
+++ db/seed.rb
@@ -1,1 +1,32 @@
-# Parse the CSV and seed the database here! Run 'ruby db/seed' to execute this code.
+# Parse the CSV and seed the database here! Run 'ruby database/seed' to execute this code.
+require "sqlite3"
+require 'csv'
+require "pry"
+
+database = SQLite3::Database.new(":memory:")
+database = {:conn => SQLite3::Database.new("JohnStewartShow")}
+
+# Create a database
+show_table = <<-SQL
+  create table JohnStewartShow (
+    id integer primary key,
+    year integer,
+    occupation text,
+    show_date text,
+    category text,
+    name text
+    );
+  SQL
+  
+database[:conn].execute(show_table)
+
+csv = File.read('daily_show_guests.csv') # couldn't get CSV.read to work :(
+
+CSV.parse(csv, headers: true).each_with_index do |row, index|
+  input = <<-SQL
+    INSERT INTO JohnStewartShow (id, year, occupation, show_date, category, name)
+    VALUES (?,?,?,?,?,?)
+    SQL
+  database[:conn].execute(input, index, row.fields)
+end
+
--- lib/queries.rb
+++ lib/queries.rb
@@ -2,4 +2,13 @@
 
 def guest_with_most_appearances
   # write your query here!
+  query1 = <<-SQL 
+  SELECT name, COUNT(id) 
+  FROM JohnStewartShow 
+  GROUP BY name 
+  ORDER BY COUNT(name) DESC LIMIT 1;
+  SQL
+  database[:conn].execute(query1)
 end
+
+
